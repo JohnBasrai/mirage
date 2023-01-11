ad0b7dd7a11ffe1b06138d30e937351a00d58dce
John Basrai <John.Basrai@vts-i.com>
2023-01-10 21:58:19 -0800

    Use macros to remove some repeated code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
-
-use clap::{Subcommand, Parser};
+use clap::{Parser, Subcommand};
 
 #[derive(Debug, Parser)]
 struct Args {
@@ -16,9 +15,8 @@ fn main() -> Result<()> {
 
 #[derive(Debug, Subcommand)]
 enum Command {
-
     /// blur an image by a given percentage
-    Blur      {
+    Blur {
         infile: String,
         outfile: String,
         value: f32,
@@ -41,97 +39,135 @@ enum Command {
         height: u32,
     },
 
-
     /// rotate an image by the given degrees, valid values 90, 180 or 270
     Rotate {
-        infile:  String,
+        infile: String,
         outfile: String,
         degrees: u32,
     },
 
     /// generate a new image in outfile
-    Generate  {
-        outfile: String,
-        value:   i32
-    },
+    Generate { outfile: String, value: i32 },
 
     /// invert an image from infile to outfile
-    Invert {
-        infile:  String,
-        outfile: String,
-    },
+    Invert { infile: String, outfile: String },
 
     /// convert an image to grey scale
-    Grayscale {
-        infile:  String,
-        outfile: String,
-    },
+    Grayscale { infile: String, outfile: String },
 
     /// generate a fractal image in the file provided.
     Fractal {
         outfile: String,
-        width:  u32,
-        height: u32
+        width: u32,
+        height: u32,
     },
+}
+
+macro_rules! imageop {
+    ($file: ident, $op: ident, $arg: expr) => {{
+        let img = (image::open(&$file).context(format!("Failed to open {}", $file))?);
+        img.$op($arg)
+    }};
+    ($file: ident, $op: ident) => {{
+        let img = (image::open(&$file).context(format!("Failed to open {}", $file))?);
+        img.$op()
+    }};
+}
+
+macro_rules! imageop_mut {
+    ($file:  ident,
+     $op:    ident $(, $args: ident)*) => {{
+         let mut img = (image::open(&$file).context(format!("Failed to open {}", $file))?);
+         img.$op($($args),*);
+         img
+    }};
 }
 
 impl Command {
     fn execute(self) -> Result<()> {
         match self {
-            Self::Blur {infile, outfile, value} => {
-                let img = image::open(&infile).context(format!("Failed to open {}", infile))?;
-                let img = img.blur(value);
-                img.save(&outfile).context(format!("Failed writing {}.", outfile))
-            },
+            Self::Blur {
+                infile,
+                outfile,
+                value,
+            } => {
+                let img = imageop!(infile, blur, value);
+                img.save(&outfile)
+                    .context(format!("Failed writing {}.", outfile))
+            }
 
-            Self::Brighten {infile, outfile, amount} => {
-                let img = image::open(&infile).context(format!("Failed to open {}", infile))?;
-                let img = img.brighten(amount);
-                img.save(&outfile).context(format!("Failed writing {}.", outfile))
-            },
+            Self::Brighten {
+                infile,
+                outfile,
+                amount,
+            } => {
+                let img = imageop!(infile, brighten, amount);
+                img.save(&outfile)
+                    .context(format!("Failed writing {}.", outfile))
+            }
 
-            Self::Crop {infile, outfile, x, y, width, height} => {
+            Self::Crop {
+                infile,
+                outfile,
+                x,
+                y,
+                width,
+                height,
+            } => {
                 let mut img = image::open(&infile).context(format!("Failed to open {}", infile))?;
                 let img = img.crop(x, y, width, height);
-                img.save(&outfile).context(format!("Failed writing {}.", outfile))
-            },
+                img.save(&outfile)
+                    .context(format!("Failed writing {}.", outfile))
+            }
 
-            Self::Rotate {infile, outfile, degrees} => {
-                let img = image::open(&infile).context(format!("Failed to open {}", infile))?;
-                let img = img.huerotate(degrees as i32);
-                let img =
-                    match degrees {
-                         90 => img.rotate90(),
-                        180 => img.rotate180(),
-                        270 => img.rotate180(),
-                        _   => return Err(
-                            anyhow::anyhow!("Invalid rotation value:{} use 90, 180 or 270", degrees)),
-                    };
-                img.save(&outfile).context(format!("Failed writing {}.", outfile))
-            },
-            Self::Invert {infile, outfile} => {
-                let mut img = image::open(&infile).context(format!("Failed to open {}", infile))?;
-                img.invert();
-                img.save(&outfile).context(format!("Failed writing {}.", outfile))
-            },
+            Self::Rotate {
+                infile,
+                outfile,
+                degrees,
+            } => {
+                let img = imageop!(infile, huerotate, degrees as i32);
+                let img = match degrees {
+                    90 => img.rotate90(),
+                    180 => img.rotate180(),
+                    270 => img.rotate180(),
+                    _ => {
+                        return Err(anyhow::anyhow!(
+                            "Invalid rotation value:{} use 90, 180 or 270",
+                            degrees
+                        ))
+                    }
+                };
+                img.save(&outfile)
+                    .context(format!("Failed writing {}.", outfile))
+            }
+            Self::Invert { infile, outfile } => {
+                let img = imageop_mut!(infile, invert);
+                img.save(&outfile)
+                    .context(format!("Failed writing {}.", outfile))
+            }
 
-            Self::Grayscale {infile, outfile} => {
-                let img = image::open(&infile).context(format!("Failed to open {}", infile))?;
-                let img = img.grayscale();
-                img.save(&outfile).context(format!("Failed writing {}.", outfile))
-            },
+            Self::Grayscale { infile, outfile } => {
+                let img = imageop!(infile, grayscale);
+                img.save(&outfile)
+                    .context(format!("Failed writing {}.", outfile))
+            }
 
-            Self::Fractal {outfile, width, height} => { fractal(&outfile, width, height) },
+            Self::Fractal {
+                outfile,
+                width,
+                height,
+            } => fractal(&outfile, width, height),
 
-            Self::Generate {outfile, value} => { generate(&outfile, value) },
-
+            Self::Generate { outfile, value } => generate(&outfile, value),
         } // match
     } // fn execute
 }
 
 fn generate(outfile: &String, color: i32) -> Result<()> {
-
-    println!("\nGenerate: file={}, color={} is not yet implemented", outfile, color);
+    println!(
+        "\nGenerate: file={}, color={} is not yet implemented",
+        outfile, color
+    );
     Ok(())
     // TODO impl
     // Create an ImageBuffer -- see fractal() for an example
@@ -150,7 +186,6 @@ fn generate(outfile: &String, color: i32) -> Result<()> {
 
 // This code was adapted from https://github.com/PistonDevelopers/image
 fn fractal(outfile: &String, width: u32, height: u32) -> Result<()> {
-
     println!("fractal: f:{outfile}, w:{width}, h:{height}");
     let mut imgbuf = image::ImageBuffer::new(width, height);
 
